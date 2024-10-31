00b335e2bf800fc9c4948a917b1f4ed853bc0e8d
Tim <tim@egirl.ceo>
2024-10-31 12:33:54 +0100

    Add: tech comps and tech badges as selection

---
+++ src/components/KeyWords.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        keyWords: string[];
+    }>();
+
+    const keyWords = props.keyWords;
+</script>
+
+<template>
+    <div class="flex items-center gap-3">
+        <div v-for="(keyWord, index) in Object.entries(keyWords)" :key="index">
+            <div class="text-base" v-if="keyWord[1] === 1">
+                {{ keyWord[0] }}
+            </div>
+            <div class="text-xl" v-else-if="keyWord[1] === 2">
+                {{ keyWord[0] }}
+            </div>
+            <div class="text-2xl" v-else>
+                {{ keyWord[0] }}
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/lib/index.js
@@ -0,0 +1,18 @@
+export const PROJECTS = [
+    {
+        "title": "Clockwork Systems - Homepage",
+        "description": "A homepage for Clockwork Systems, a software development company.",
+        "keyWords": ["Tailwind", "NextJS", "Docker"]
+    },
+    {
+        "title": "TipTalk",
+        "description": "A web application that allows users ask and answer questions while earning money.",
+        "keyWords": ["Tailwind", "SvelteKit", "Docker", "Pocketbase", "Stripe", "OAuth2"]
+    },
+    {
+        "title": "Devotee",
+        "description": "A platform for sharing and monetizing user created content.",
+        "keyWords": ["Tailwind", "SvelteKit", "Docker", "Django", "REST", "S3"]
+    }
+
+]
