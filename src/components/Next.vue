aa7907058ce992921936df749fb4b6c79adfae6a
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-11-29 21:10:12 +0100

    Add: footer & legal stuff

---
+++ src/components/Next.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const props = withDefaults(defineProps<{
+        href?: string;
+        onClick: any;
+    }>(), {
+        href: "",
+        onClick: null
+    });
+
+    const href = ref<string>(props.href);
+    const handleClick = () => {
+        if (typeof props.onClick === 'function') {
+            props.onClick();
+        }
+    };
+</script>
+
+<template>
+    <a class="hover:scale-105 duration-100 ease-linear" :href="href" @click="handleClick">
+      <div class="flex">
+        <svg width="67" height="53" viewBox="0 0 67 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M31 1L64 25L31 52" stroke="black" stroke-width="2"/>
+          <path d="M17 1L50 25L17 52" stroke="black" stroke-width="1.5"/>
+          <path d="M3 1L36 25L3 52" stroke="black"/>
+        </svg>            
+      </div>
+    </a>
+</template>
